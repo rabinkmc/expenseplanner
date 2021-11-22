f3f2dd8dc254020c9c07689afee5f30022531825
Rabin Dhamala <rabin.dhamala@aayulogic.com>
2021-11-22 17:59:00 +0545

    initial commit

---
+++ expense.py
@@ -0,0 +1,16 @@
+from collections import deque, OrderedDict
+
+input = {'budget': 10000, 'saving':3000 } 
+priorites = [ ('rent', 3000),
+              ('food', 2000), 
+              ('hook',3000),
+              ('battery', 4999) ] 
+ 
+can_buy = [ ] 
+remaining_budget = input['budget']
+
+while remaining_budget > saving:
+    if priority[1] < can_spend : 
+        can_buy.append(priority)
+    
+
